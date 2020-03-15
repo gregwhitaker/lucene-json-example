50795769d633a6bd1c258921794f8206a60915ac
gregwhitaker <gwhitake@gmail.com>
2020-03-15 10:17:13 -0700

    Doing work

--- src/main/java/example/lucene/Application.java
+++ src/main/java/example/lucene/Application.java
@@ -1,6 +1,10 @@
 package example.lucene;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Application {
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
     public static void main(String... args) {
 
