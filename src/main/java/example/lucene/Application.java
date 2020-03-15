8d972a5847473ef06c499bd49ca11f9a822e7663
gregwhitaker <gwhitake@gmail.com>
2020-03-15 10:25:22 -0700

    Doing work

--- src/main/java/example/lucene/Application.java
+++ src/main/java/example/lucene/Application.java
@@ -1,5 +1,6 @@
 package example.lucene;
 
+import example.lucene.json.JsonDocumentParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,6 +8,6 @@ public class Application {
     private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
     public static void main(String... args) {
-
+        JsonDocumentParser parser = new JsonDocumentParser(args[0]);
     }
 }
---
+++ src/main/java/example/lucene/indexing/JsonIndexWriter.java
@@ -0,0 +1,31 @@
+package example.lucene.indexing;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JsonIndexWriter {
+    private static final Logger LOG = LoggerFactory.getLogger(JsonIndexWriter.class);
+
+    private final String idxDirPath;
+
+    public JsonIndexWriter(final String idxDirPath) {
+        this.idxDirPath = idxDirPath;
+    }
+
+    public void create() {
+
+    }
+
+    public void open() {
+
+    }
+
+    public void addDocument(JsonNode node) {
+
+    }
+
+    public void close() {
+
+    }
+}
